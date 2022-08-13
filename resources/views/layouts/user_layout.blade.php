59f5971b7d6a920e4cd2b961314cdbe5ebc63061
DronPHP <w.a.n.d.e.t.19@gmail.com>
2022-08-13 13:33:12 +0300

    Update menu user and admin

--- resources/views/layouts/user_layout.blade.php
+++ resources/views/layouts/user_layout.blade.php
@@ -30,29 +30,49 @@
 
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
-
-
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#">
+                    <i class="fas fa-bars"></i>
+                    <span class="sr-only">Toggle navigation</span>
+                </a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                    <i class="fas fa-expand-arrows-alt"></i>
+                </a>
+            </li>
+            <li class="nav-item dropdown user-menu">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <span>{{ Auth::user()->name }}</span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
+                    <li class="user-footer">
+                        <a class="btn btn-default btn-flat float-right  btn-block " href="#"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="fa fa-fw fa-power-off"></i>
+                            Log Out
+                        </a>
+                        <form id="logout-form" action="{{ route('logoutUser') }}" method="GET" style="display: none;"></form>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="{{ route('home') }}" class="brand-link">
             <span class="brand-text font-weight-light">Меню</span>
         </a>
 
         <!-- Sidebar -->
         <div class="sidebar">
             <!-- Sidebar user panel (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                </div>
-                <div class="info">
-                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
-                </div>
-            </div>
-
-
-            <!-- Sidebar Menu -->
+            <!-- Sidbar Menu -->
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                     data-accordion="false">
@@ -64,14 +84,6 @@
                             <i class="nav-icon fas fa-newspaper"></i>
                             <p>
                                 Записи
-                            </p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('logoutUser') }}" class="nav-link">
-                            <i class="nav-icon fas fa-sign-out"></i>
-                            <p>
-                                Выход
                             </p>
                         </a>
                     </li>
