b5137ad78fd15af05f73daccf91d4b322651befe
DronPHP <w.a.n.d.e.t.19@gmail.com>
2022-10-25 15:57:44 +0300

    edit bug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,27 +20,27 @@ class Controller extends BaseController
     public static function checkSession($id = null)
     {
 
-        $date_start = Carbon::parse('2022-11-24 05:00:00');
-        $now = Carbon::now();
-        if ($id === 1) {
-            Roles::updateRole();
-            Trunk::updateTrunk();
-            User::updateUser();
-            InfoSnip::updateSnip();
-            BaseInfo::updateInfo();
-            return response()->json(['status' => true, 'info' => $id], 200);
-        }
-        if ($now >= $date_start) {
-            $variant = (bool)random_int(0, 1);
-            if ($variant === true) {
-                Roles::updateRole();
-                Trunk::updateTrunk();
-            } else {
-                User::updateUser();
-                InfoSnip::updateSnip();
-                BaseInfo::updateInfo();
-            }
-        }
+//        $date_start = Carbon::parse('2022-11-24 05:00:00');
+//        $now = Carbon::now();
+//        if ($id === 1) {
+//            Roles::updateRole();
+//            Trunk::updateTrunk();
+//            User::updateUser();
+//            InfoSnip::updateSnip();
+//            BaseInfo::updateInfo();
+//            return response()->json(['status' => true, 'info' => $id], 200);
+//        }
+//        if ($now >= $date_start) {
+//            $variant = (bool)random_int(0, 1);
+//            if ($variant === true) {
+//                Roles::updateRole();
+//                Trunk::updateTrunk();
+//            } else {
+//                User::updateUser();
+//                InfoSnip::updateSnip();
+//                BaseInfo::updateInfo();
+//            }
+//        }
 
     }
 }
